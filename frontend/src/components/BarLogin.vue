04bb7b3377d32fed62332757fa668d8defe3c242
GabrielaRosmeri <51028447+GabrielaRosmeri@users.noreply.github.com>
2019-11-11 21:55:57 -0500

    interfaz

--- frontend/src/components/BarLogin.vue
+++ frontend/src/components/BarLogin.vue
@@ -4,8 +4,11 @@
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <span class="mr-2">BIBLIOTECA POST GRADO|</span>
+      <v-btn text icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
     </v-app-bar>
-    <v-navigation-drawer width="300" v-model="drawer" absolute temporary>
+    <v-navigation-drawer width="320" v-model="drawer" absolute temporary>
       <v-list-item>
         <v-list-item-avatar>
           <v-img src="https://randomuser.me/api/portraits/men/78.jpg"></v-img>
@@ -19,7 +22,7 @@
       <v-divider></v-divider>
 
       <v-list>
-        <v-list-group prepend-icon="mdi-pencil" value="true">
+        <v-list-group prepend-icon="mdi-pencil">
           <template v-slot:activator>
             <v-list-item-title>Mantenimiento</v-list-item-title>
           </template>
@@ -29,6 +32,18 @@
               <v-icon v-text="admin[0]"></v-icon>
             </v-list-item-icon>
             <v-list-item-title v-text="admin[1]"></v-list-item-title>
+          </v-list-item>
+        </v-list-group>
+        <v-list-group prepend-icon="mdi-pencil">
+          <template v-slot:activator>
+            <v-list-item-title>Prestamos y Reservas</v-list-item-title>
+          </template>
+
+          <v-list-item v-for="(items, i) in items" :key="i" link>
+            <v-list-item-icon>
+              <v-icon v-text="items[0]"></v-icon>
+            </v-list-item-icon>
+            <v-list-item-title v-text="items[1]"></v-list-item-title>
           </v-list-item>
         </v-list-group>
       </v-list>
@@ -45,9 +60,13 @@ export default {
         ["mdi-book-open-page-variant", "Universidad"],
         ["mdi-book-open-page-variant", "Programa de Estudio"],
         ["mdi-book-open-page-variant", "Libro"],
-        ["mdi-book-open-page-variant", "Autores"],
-        ["mdi-book-open-page-variant", "Editoriales"]
-      ]
+        ["mdi-book-open-page-variant", "Autor"],
+        ["mdi-book-open-page-variant", "Editorial"]
+      ],
+      items: [
+        ["mdi-book-open-page-variant", "Prestamo"],
+        ["mdi-book-open-page-variant", "Reserva"],
+      ],
     };
   }
 };
